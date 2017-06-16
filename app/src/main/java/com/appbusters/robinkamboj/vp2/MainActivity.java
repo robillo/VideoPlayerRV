d68f1a78874d929d1cecc483655da3ccfa534348
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-16 17:51:38 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/vp2/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/vp2/MainActivity.java
@@ -26,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
         Log.e("ONCREATE", "MA");
 
         recyclerView = (AAH_CustomRecyclerView) findViewById(R.id.rv_home);
+
         Picasso p = Picasso.with(this);
+
+        //todo before setAdapter
+        recyclerView.setActivity(this);
 
         modelList.add(new MyModel("https://youtu.be/HRzzglgC180", "https://1.bp.blogspot.com/-KOrgmRC8Nj0/WSh6_7z7ryI/AAAAAAAAASc/_O4UwRnmDsAZ2IFzIAh_eKXuVh9gti_CACK4B/s320/yoga2.jpg", "MP4 VIDEO"));
         modelList.add(new MyModel("http://www.sample-videos.com/video/flv/240/big_buck_bunny_240p_2mb.flv", "https://3.bp.blogspot.com/-O-kG5WpcVa8/WSh-Fh2m0_I/AAAAAAAAAS8/TwAT0D23cx0sQ0oCu_cKkPVL8MdAACtWgCK4B/s320/Buddha%2BSpiritual%2BBuddha%2B%2BAbstract%2B3D%2Band%2BCG%2BHD%2BDesktop%2BWallpaper.jpg", "FLV VIDEO"));
@@ -40,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        //todo before setAdapter
-        recyclerView.setActivity(this);
 
         //optional - to play only first visible video
         recyclerView.setPlayOnlyFirstVideo(true); // false by default
